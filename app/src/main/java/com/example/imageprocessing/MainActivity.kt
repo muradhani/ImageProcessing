620340c6e5cb63b6099153ddd0f146ed41439799
murad <mouradhany574@gmail.com>
2024-04-22 22:18:29 +0200

    zoom in and out works well

--- app/src/main/java/com/example/imageprocessing/MainActivity.kt
+++ app/src/main/java/com/example/imageprocessing/MainActivity.kt
@@ -4,36 +4,40 @@ import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.view.View
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.children
 import androidx.databinding.DataBindingUtil
 import com.example.imageprocessing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this , R.layout.activity_main)
-
         binding.openGalleryBtn.setOnClickListener {
-            // Launch the photo picker activity
             pickMediaLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
     }
-    // Activity result launcher for picking media
+
     private val pickMediaLauncher = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-        // Callback is invoked after the user selects a media item or closes the photo picker.
         if (uri != null) {
             Log.d("PhotoPicker", "Selected URI: $uri")
+            binding.image.setImageURI(uri)
         } else {
             Log.d("PhotoPicker", "No media selected")
         }
     }
-
 
 }
